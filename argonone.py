28a0de9e66f952726e3914017da18639653a8f94
Jian Li <lijianxp2005@gmail.com>
2021-02-01 03:14:14 -0500

    enable Raspberry Pi hardware code

--- argonone.py
+++ argonone.py
@@ -1,5 +1,5 @@
-# import smbus
-# import RPi.GPIO as GPIO
+import smbus
+import RPi.GPIO as GPIO
 import yaml
 
 import os
@@ -25,26 +25,38 @@ class Config:
             raise ValueError("value of 'mode' must be 'balanced', 'quiet', or 'performance'")
 
 class PiHardware:
-    @staticmethod
-    def temperature():
-        return max(PiHardware.cpu_temperature(), PiHardware.gpu_temperature())
+    FAN_SPEED_BUS_ADDRESS = 0x1a
 
-    @staticmethod
-    def cpu_temperature():
+    def __init__(self):
+        if GPIO.RPI_REVISION in [2, 3]:
+            self.bus = smbus.SMBus(1)
+        else:
+            self.bus = smbus.SMBus(0)
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+
+    def temperature(self):
+        return max(self.cpu_temperature(), self.gpu_temperature())
+
+    def cpu_temperature(self):
         temperatures = psutil.sensors_temperatures()
         if not 'cpu_thermal' in temperatures:
             return None
         return temperatures['cpu_thermal'][0].current
 
-    @staticmethod
-    def gpu_temperature():
+    def gpu_temperature(self):
         output = subprocess.check_output(['vcgencmd', 'measure_temp'], encoding='utf-8')
         return float(output.replace('temp=', '').replace('\'C\n', ''))
+    
+    def set_fan_speed(self, percent):
+        self.bus.write_byte(self.FAN_SPEED_BUS_ADDRESS, percent)
 
-def start_fan_service(config):
-    print(PiHardware.cpu_temperature())
-    print(PiHardware.gpu_temperature())
-    print(PiHardware.temperature())
+
+def start_fan_service(pi, config):
+    print(pi.cpu_temperature())
+    print(pi.gpu_temperature())
+    print(pi.temperature())
+    pi.set_fan_speed(10)
 
 
 def start_button_service():
@@ -61,15 +73,15 @@ def stop_button_service():
 
 def main():
     config = Config(CONFIG_FILE)
+    pi = PiHardware()
     try:
-        start_fan_service(config)
+        start_fan_service(pi, config)
         start_button_service()
     except:
         stop_fan_service()
         stop_button_service()
     finally:
-        pass
-        # GPIO.cleanup()
+        GPIO.cleanup()
 
 
 if __name__ == "__main__":
