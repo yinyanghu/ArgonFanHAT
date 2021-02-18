bec1f1d2ba6f6f7b884832e7d2d6b4aa6a1f0b2f
Jian Li <lijianxp2005@gmail.com>
2021-02-17 23:42:47 -0500

    add an install script for Raspberry Pi

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git clone https://github.com/yinyanghu/ArgonFanHAT.git /tmp
+
+pip install smbus2 RPi.GPIO psutil
+
+basedir="/tmp/ArgonFanHAT"
+
+mkdir -p /etc/argonone
+cp "${basedir}/config.yaml" /etc/argonone/
+cp "${basedir}/argonone.service" /lib/systemd/system/
+cp "${basedir}/argonone.py" /usr/bin/
+
+chmod 644 /lib/systemd/system/argonone.service
+
+systemctl daemon-reload
+systemctl enable argonone.service
+systemctl start argonone.service
