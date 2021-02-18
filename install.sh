600d60595e7ebc1fb7a108135e11934ac08038e5
Jian Li <lijianxp2005@gmail.com>
2021-02-18 00:11:47 -0500

    clean up after done

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-git clone https://github.com/yinyanghu/ArgonFanHAT.git /tmp/ArgonFanHAT
+basedir="/tmp/ArgonFanHAT"
+
+rm -rf "$basedir"
+git clone https://github.com/yinyanghu/ArgonFanHAT.git "$basedir"
 
 pip install smbus2 RPi.GPIO psutil
 
-basedir="/tmp/ArgonFanHAT"
-
 mkdir -p /etc/argonone
-cp "${basedir}/config.yaml" /etc/argonone/
-cp "${basedir}/argonone.service" /lib/systemd/system/
-cp "${basedir}/argonone.py" /usr/bin/
+cp "$basedir/config.yaml" /etc/argonone/
+cp "$basedir/argonone.service" /lib/systemd/system/
+cp "$basedir/argonone.py" /usr/bin/
 
 chmod 644 /lib/systemd/system/argonone.service
 
 systemctl daemon-reload
 systemctl enable argonone.service
 systemctl start argonone.service
+
+rm -rf "$basedir"
